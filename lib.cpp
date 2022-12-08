2dd3ff5fefa1879a7f465cf019b653e6f0faef1a
mattemonte03 <117922070+mattemonte03@users.noreply.github.com>
2022-12-08 16:15:13 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
-
+bool input (char &let){
+    if(let>=65&&let<=90) {
+        let = let + 32;
+        return true;
+    }else if(let>=97 && let<=122){
+        let=let-32;
+        return true;
+    }else{
+        return false;
+    }}
